46520fa48cef181f560042e41a59c54802b504f0
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-11-29 22:54:22 +0530

    redesign code

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Navbar from '@/components/landing/Navbar'
 import Hero from '@/components/landing/Hero'
-import DesignedForDevelopers from '@/components/landing/DesignedForDevelopers'
+import Features from '@/components/landing/Features'
 import CompactAccordion from '@/components/landing/CompactAccordion'
+import CallToAction from '@/components/landing/CallToAction'
 import Footer from '@/components/landing/Footer'
 
 function page() {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen bg-background selection:bg-primary/20'>
       <Navbar />
-      <Hero />
-      <DesignedForDevelopers />
-      <CompactAccordion />
+      <main>
+        <Hero />
+        <Features />
+        <CompactAccordion />
+        <CallToAction />
+      </main>
       <Footer />
     </div>
   )
